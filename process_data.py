cf7bf17b580fce3e057067098252487c2032809c
Alex Payne <alex.payne@renishaw.com>
2019-12-12 13:00:32 +0000

    Initial commit

---
+++ process_data.py
@@ -0,0 +1,43 @@
+import os
+
+outdir = "processed_data\\"
+indir = "raw_data\\"
+
+for filename in os.listdir(indir):
+
+    filestream = open(indir + filename, "r")
+
+    fileContents = filestream.read()
+
+    fileContents = fileContents.replace("repeat chorus", "")
+    fileContents = fileContents.replace("Repeat Chorus", "")
+    fileContents = fileContents.replace("(Chorus)", "")
+    fileContents = fileContents.replace("Chorus\n", "")
+    fileContents = fileContents.replace("(Refrain)", "")
+    fileContents = fileContents.replace("Refrain\n", "")
+    fileContents = fileContents.replace("(Repeat)", "")
+    fileContents = fileContents.replace("Repeat\n", "")
+
+    fileContents = fileContents.replace("repeat chorus", "")
+
+    fileContents = fileContents.replace("\n\n\n", "\n\n")
+    fileContents = fileContents.replace("\n\n\n", "\n\n")
+    fileContents = fileContents.replace("\n\n", "\n")
+    fileContents = fileContents.replace("\n\n", "\n")
+    
+    fileContents = fileContents.replace("â€™", "'")
+    fileContents = fileContents.replace("â€˜", "'")
+
+    print(filename)
+    outstream = open(outdir + filename, "w")
+
+    outstream.write(fileContents)
+
+    outstream.close()
+    filestream.close()
+
+
+with open("tensor.txt", "w+") as outstream:
+    for filename in os.listdir(outdir):
+        with open(outdir + filename) as file:
+            outstream.write(file.read())
